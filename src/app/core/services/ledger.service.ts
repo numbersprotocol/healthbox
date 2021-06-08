e74e7d87850661f16c10ef9800356a8d340a9ab8
DaYuan <sduum82@gmail.com>
2021-06-08 21:56:44 +0800

    add Web CryptoApi Signature

--- src/app/core/services/ledger.service.ts
+++ src/app/core/services/ledger.service.ts
@@ -1,29 +1,38 @@
 import { Injectable } from '@angular/core';
 import { sendMessage } from '@numbersprotocol/niota';
-import { from, Observable, of } from 'rxjs';
-import { catchError, tap, timeout } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { catchError, map, switchMap, take, tap, timeout } from 'rxjs/operators';
 import { WebCryptoApiSignatureProvider } from '../../core/services/web-crypto-api-signature-rovider.service'
 
 @Injectable({
   providedIn: 'root',
 })
 export class LedgerService {
-  constructor() {}
+  constructor(
+    private readonly CryptoSignature: WebCryptoApiSignatureProvider
+  ) {
+    this.CryptoSignature.initialize();
+  }
 
   private registerOnLedger(hash: string): Observable<any> {
     const index = 'Lifebox';
-    const dummySignature = '';
     const rawMsg = { hash };
-    return from(sendMessage(index, rawMsg, dummySignature)).pipe(
+    return this.CryptoSignature.getCryptoData().pipe(
+      map((cryptoData) => {
+        return cryptoData;
+      }),
+      switchMap((dummySignature) => sendMessage(index, rawMsg, dummySignature.publicKey)),
       timeout(10000),
-      tap(resultHash =>
+      tap(resultHash => {
         console.log(`Message ID ${resultHash} registered on ledger`)
+      }
       ),
       catchError(err => {
         console.log(err);
         return of('');
       })
-    );
+      
+    )
   }
 
   createTransactionHash(hash): Observable<string> {
