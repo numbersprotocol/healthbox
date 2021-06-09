21a509269719442e4d1a6708c527a2030337c4bd
DaYuan <sduum82@gmail.com>
2021-06-09 18:08:28 +0800

    remove console

--- src/app/core/services/crypto/crypto.ts
+++ src/app/core/services/crypto/crypto.ts
@@ -3,13 +3,10 @@ import Web3 from "web3";
 const web3 = new Web3();
 
 export function createEthAccount() {
-  console.log("02");
   return web3.eth.accounts.create();
 }
 
 export function loadEthAccount(privateKey: string) {
-  console.log("loadEthAccount-02");
-
   return web3.eth.accounts.privateKeyToAccount(privateKey);
 }
 
